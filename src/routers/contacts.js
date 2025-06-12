081fd51616b3f31a29a41ab90a3ebb7b70ab4340
Yuliia Fomenko <yaulia3@gmail.com>
2025-06-12 13:42:24 +0100

    step 1-2 done

---
+++ src/routers/contacts.js
@@ -0,0 +1,13 @@
+import { Router } from 'express';
+import { getAllContactsController, getContactByIdController } from '../controllers/contacts.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+
+const contactsRouter = Router();
+
+contactsRouter.get('/contacts', ctrlWrapper(getAllContactsController));
+contactsRouter.get(
+  '/contacts/:contactId',
+  ctrlWrapper(getContactByIdController),
+);
+
+export default contactsRouter;
--- src/server.js
+++ src/server.js
@@ -3,7 +3,9 @@ import pino from 'pino-http';
 import cors from 'cors';
 import { getEnvVar } from './utils/getEnvVar.js';
 import { ENV_VARS } from './constants/envVars.js';
-import { getAllContacts, getContactById } from './services/contacts.js';
+import contactsRouter from './routers/contacts.js';
+import { errorHandlerMiddleware } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
 export const setupServer = () => {
   const app = express();
@@ -17,39 +19,11 @@ export const setupServer = () => {
     }),
   );
 
-  app.get('/contacts', async(req, res) => {
-    const contacts = await getAllContacts();
-    res.json(
-      {
-        status: 200,
-        message: "Successfully found contacts!",
-        data: contacts,
-      }
-    );
-  });
-
-  app.get('/contacts/:contactId', async (req, res) => {
-    const {contactId} = req.params;
-    const contact = await getContactById(contactId);
-
-    if(!contact){
-      return res.status(404).json({
-        message: 'Contact not found',
-      });
-    }
-
-    res.json({
-      status: 200,
-      message: `Successfully found contact with id ${contactId}!`,
-      data: contact,
-    });
-  });
-
-  app.use((req, res, next) => {
-    res.status(404).json({
-        message: 'Not found',
-      });
-  });
+  app.use(contactsRouter);
+  
+  app.use(errorHandlerMiddleware);
+  
+  app.use(notFoundHandler);
 
   const PORT = getEnvVar(ENV_VARS.PORT, 3000);
 
